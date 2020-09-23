68d0b7105cdcdecac1857df71902fc93a9c2e6ae
pavan1585 <69182052+pavan1585@users.noreply.github.com>
2020-09-23 17:36:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('pavan kalyan
+                regno:17mis1131
+                course:cloud computing')
 })
 
 app.listen(app.get('port'), function() {
