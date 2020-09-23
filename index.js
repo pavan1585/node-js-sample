398adce6d290e15cbda9459b913b4a8a87a04bb8
pavan1585 <69182052+pavan1585@users.noreply.github.com>
2020-09-23 17:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Reg : 17MIS1131, Name : Pavan')
 })
 
 app.listen(app.get('port'), function() {
